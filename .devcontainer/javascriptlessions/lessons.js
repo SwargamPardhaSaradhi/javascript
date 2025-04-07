c310027737c50f73951bef863be5cff31cd1f41a
SwargamPardhaSaradhi <swargampardha@gmail.com>
2025-04-07 13:42:39 +0000

    continuing

--- .devcontainer/javascriptlessions/lessons.js
+++ .devcontainer/javascriptlessions/lessons.js
@@ -8,4 +8,26 @@
 // // rest we can do it we can change rest variables like let and var
 // console.table([account,account_email,acc_pass,account_city]);
 
-//   Data types -- 2
+// //   Data types -- 2
+// let age = 18;
+// let name = "kit";
+// let can_login = false;
+// let some = 10.0;
+// // we have null in java script too
+// // undefined is a type in which when we wont define something 
+// console.table([typeof age,typeof name,typeof can_login,typeof some]);
+
+// // we use typeof for knowing the type of the datatype like number etc....
+
+// //    Data Type Conversions -- 3
+// let str = "33";
+// console.log(typeof str);
+// let num = Number(str);
+// console.log(typeof num);
+// let a = String(num);
+// console.log(typeof (a));
+// // if the conversion doesnt match it will give us nan
+// /*  if we have string as 123ab
+// and we try to convert to number then it gives us NaN (not an number)
+// */ 
+
