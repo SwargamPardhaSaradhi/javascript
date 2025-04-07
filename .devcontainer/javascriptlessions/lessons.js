3dde4c0454cbaab937a0a2ca3b53e469431e883c
SwargamPardhaSaradhi <swargampardha@gmail.com>
2025-04-07 13:27:54 +0000

    adding

---
+++ .devcontainer/javascriptlessions/lessons.js
@@ -0,0 +1,11 @@
+// //      variables -- 1
+// const account = 10; // const cant be changed again once it is initilized 
+// let account_email = "kittu@gmail.com"; // we mostly use let in this 
+// var acc_pass = "12345"; // we wont use var mostly due to scope problem as it has functinoal scope problem
+// account_city = "jaipur"; // it is not allowed but it is valid
+
+// acc_pass = "231";// we can change var and let at anywhere in the scope 
+// // rest we can do it we can change rest variables like let and var
+// console.table([account,account_email,acc_pass,account_city]);
+
+//   Data types -- 2
